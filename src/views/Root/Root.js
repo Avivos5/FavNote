d46a93cfd6d430b00a65a317d43a1850a0c60d25
Mateusz Grabowski <mategra5@gmail.com>
2021-01-10 20:23:02 +0100

    dodanie styled-components i absolute path

---
+++ src/views/Root/Root.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from 'components/Button/Button';
+
+const Root = () => (
+    <>
+        <h1>Hello World</h1>
+        <Button>Siemanko</Button>
+    </>
+);
+
+export default Root;
