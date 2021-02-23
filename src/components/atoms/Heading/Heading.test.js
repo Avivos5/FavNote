6e1f6afb825f91b4e6b7b3bc622275b454e84d2a
Mateusz Grabowski <mategra5@gmail.com>
2021-02-23 23:58:06 +0100

    dodane podstawy testów

---
+++ src/components/atoms/Heading/Heading.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import Heading from './Heading';
+import { theme } from 'theme/mainTheme';
+
+describe('Heading Component', () => {
+    it('Renders child text', () => {
+        const { getByText } = render(
+            <ThemeProvider theme={theme}>
+                <Heading>Hello</Heading>
+            </ThemeProvider>
+        );
+        getByText('Hello');
+    });
+});
---
+++ src/index.test.js
@@ -0,0 +1,5 @@
+const add = (a, b) => a + b;
+
+it('adds two numbers', () => {
+    expect(add(10, 5)).toBe(15);
+});
